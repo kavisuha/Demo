bd97ba01a10afb4252f729a58df41c143393991a
kavisuha <66821996+kavisuha@users.noreply.github.com>
2020-07-13 20:32:12 +0530

    Add files via upload

---
+++ interface/Test.java
@@ -0,0 +1,11 @@
+package live;
+import music.string.*;
+import music.wind.*;
+public class Test {
+    public static void main(String[] args){
+        Veena s1 = new Veena();
+        s1.play();
+        Saxophone s2 = new Saxophone();
+        s2.play();
+    }
+}
